7855ca8251d3bbd96b1cd1afcc6f8bef2e75ba6d
Humberto Freitas <jhcff88@gmail.com>
2026-06-28 22:24:14 -0300

    feat: Inicio.css

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import './Inicio.css';
+
 export default function Inicio() {
     return (
-        <div>
-            <h1>Sistema Integrado da Mineradora</h1>
-            <p>Bem-vindo ao painel de controle. Utilize o menu acima para gerenciar os
-                recursos.</p>
+        <div className="inicio-container">
+            <div className="inicio-icone">🏭</div>
+            <h1 className="inicio-titulo">Sistema Integrado da Mineradora</h1>
+            <p className="inicio-texto">
+                Bem-vindo ao painel de controle. Utilize o menu de navegação acima para gerenciar os equipamentos, cidades, funcionários e serviços com facilidade.
+            </p>
         </div>
     );
 }
